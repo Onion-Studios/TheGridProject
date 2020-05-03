af882a89f176aee854cc6d16b9bd69d586b79003
Andrea Facchinetti <facchiandrea1510@gmail.com>
2020-05-03 20:05:57 +0200

    Sistemazione particellari esistenti

---
+++ progetto comune prototipo/Assets/Scripts/Particles/RotatingParticle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingParticle : MonoBehaviour
+{
+    public Vector3 speed;
+    void Update()
+    {
+        transform.Rotate(speed * Time.deltaTime);
+    }
+}
