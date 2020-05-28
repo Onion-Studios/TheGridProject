598a413e18e1221138066dce21cbd6f8550c9444
Andrea Facchinetti <facchiandrea1510@gmail.com>
2020-05-28 20:14:32 +0200

    Tuning particles
    Colorazione e dimensione, + prove despawn uccisione

---
+++ progetto comune prototipo/Assets/08-Particles/ProvisoryScripts/NormalDeath.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalDeath : MonoBehaviour
+{
+    public GameObject enemy;
+    public GameObject fascia;
+    public float BlackToDeath = 0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("PreDeath", 3);
+    }
+
+    void PreDeath()
+    {
+        ParticleSystemManager.Instance.ActivateParticle("NormalInkDeath");
+        enemy.GetComponent<Renderer>().material.color = Color.black;
+        fascia.GetComponent<Renderer>().material.color = Color.black;
+        Invoke("Death", BlackToDeath);
+    }
+    void Death()
+    {
+        enemy.SetActive(false);
+        fascia.SetActive(false);
+    }
+}
