284c195feb3a1f52aa02db5f366c252e72ca76e8
Daniele Comotti <comotti.daniele@gmail.com>
2020-06-06 01:25:09 +0200

    Sistemazione Main menu

---
+++ progetto comune prototipo/Assets/01-Scripts/MenuWithKeyboard.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuWithKeyboard : MonoBehaviour
+{
+    public GameObject MainMenu, OptionsMenu, AudioMenu, VideoMenu, ControlsMenu, ExitDialogue, Record, PointsTag;
+    public GameObject FirstOptionsButton, CloseOptionsButton, FirstCloseButton, CloseCloseButton, AudioSlider, CloseAudioButton, VideoResolution, CloseVideoButton, FirstControlsButton, CloseControlsButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #region Options
+    public void OpenOptions()
+    {
+        OptionsMenu.SetActive(true);
+        MainMenu.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(FirstOptionsButton);
+    }
+    public void CloseOptions()
+    {
+        OptionsMenu.SetActive(false);
+        MainMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(CloseOptionsButton);
+    }
+    #endregion
+    #region Audio
+    public void OpenAudio()
+    {
+        AudioMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(AudioSlider);
+    }
+    public void CloseAudio()
+    {
+        AudioMenu.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(CloseAudioButton);
+    }
+    #endregion
+    #region Video
+    public void OpenVideo()
+    {
+        VideoMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(VideoResolution);
+    }
+    public void CloseVideo()
+    {
+        VideoMenu.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(CloseVideoButton);
+    }
+    #endregion
+    #region Controls
+    public void OpenControls()
+    {
+        ControlsMenu.SetActive(true);
+        OptionsMenu.SetActive(false);
+        Record.SetActive(false);
+        PointsTag.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(FirstControlsButton);
+    }
+    public void CloseControls()
+    {
+        ControlsMenu.SetActive(false);
+        OptionsMenu.SetActive(true);
+        Record.SetActive(true);
+        PointsTag.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(FirstControlsButton);
+    }
+    #endregion
+    #region Exit
+    public void OpenExit()
+    {
+        ExitDialogue.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(FirstCloseButton);
+    }
+    public void CloseExit()
+    {
+        ExitDialogue.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(CloseCloseButton);
+    }
+    #endregion
+}
---
+++ progetto comune prototipo/Assets/01-Scripts/SettingsMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer AudioMixer;
+    Resolution[] Resolutions;
+    public TMP_Dropdown ResolutionDropdown;
+
+    void Start()
+    {
+        Resolutions=Screen.resolutions;
+        ResolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int CurrentResolutionIndex = 0;
+        for(int i=0; i<Resolutions.Length; i++)
+        {
+            string option = Resolutions[i].width + "x" + Resolutions[i].height;
+            options.Add(option);
+
+            if(Resolutions[i].width==Screen.width && Resolutions[i].height==Screen.height)
+            {
+                CurrentResolutionIndex = i;
+            }
+        }
+        ResolutionDropdown.AddOptions(options);
+        ResolutionDropdown.value = CurrentResolutionIndex;
+        ResolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int ResolutionIndex)
+    {
+        Resolution resolution = Resolutions[ResolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetMaster (float master)
+    {
+        AudioMixer.SetFloat("Master", master);
+    }
+    public void SetMusic(float music)
+    {
+        AudioMixer.SetFloat("Music", music);
+    }
+    public void SetSFX(float sfx)
+    {
+        AudioMixer.SetFloat("SFX", sfx);
+    }
+
+    public void SetFullScreen(bool IsFullScreen)
+    {
+        Screen.fullScreen = IsFullScreen;
+    }
+}
