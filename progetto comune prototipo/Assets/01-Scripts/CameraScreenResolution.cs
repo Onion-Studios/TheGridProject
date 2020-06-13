c37f56d77cb9c5f9d77814c5d39c0783b438c4dd
Daniele Comotti <comotti.daniele@gmail.com>
2020-06-13 12:37:36 +0200

    Altre modifiche

--- progetto comune prototipo/Assets/01-Scripts/CameraScreenResolution.cs
+++ progetto comune prototipo/Assets/01-Scripts/CameraScreenResolution.cs
@@ -6,11 +6,16 @@ public class CameraScreenResolution : MonoBehaviour
 {
     public bool MaintainWidth = true;
     float DefaultWidth;
+    float DefaultHeight;
+    Vector3 CameraPos;
+    [Range(-1, 1)] public int AdaptPosition;
 
     // Start is called before the first frame update
     void Start()
     {
+        CameraPos = Camera.main.transform.position;
         DefaultWidth = Camera.main.orthographicSize * Camera.main.aspect;
+        DefaultHeight = Camera.main.orthographicSize;
     }
 
     // Update is called once per frame
@@ -19,10 +24,12 @@ public class CameraScreenResolution : MonoBehaviour
         if(MaintainWidth)
         {
             Camera.main.orthographicSize = DefaultWidth / Camera.main.aspect;
+
+            Camera.main.transform.position = new Vector3(CameraPos.x, AdaptPosition * (DefaultHeight - Camera.main.orthographicSize), CameraPos.z);
         }
         else
         {
-
+            Camera.main.transform.position = new Vector3(AdaptPosition * (DefaultWidth - Camera.main.orthographicSize*Camera.main.aspect), CameraPos.y, CameraPos.z);
         }
     }
 }
