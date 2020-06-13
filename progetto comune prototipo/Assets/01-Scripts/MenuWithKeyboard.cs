c90dedbfebf83d5ccbcb0f4c8f6f0f5a2e325e03
Daniele Comotti <comotti.daniele@gmail.com>
2020-06-13 18:52:59 +0200

    Modifiche menù principale

--- progetto comune prototipo/Assets/01-Scripts/MenuWithKeyboard.cs
+++ progetto comune prototipo/Assets/01-Scripts/MenuWithKeyboard.cs
@@ -2,23 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MenuWithKeyboard : MonoBehaviour
 {
-    public GameObject MainMenu, OptionsMenu, AudioMenu, VideoMenu, ControlsMenu, ExitDialogue, Record, PointsTag;
-    public GameObject FirstOptionsButton, CloseOptionsButton, FirstCloseButton, CloseCloseButton, AudioSlider, CloseAudioButton, VideoResolution, CloseVideoButton, FirstControlsButton, CloseControlsButton;
+    //public GameObject MainMenu, OptionsMenu, AudioMenu, VideoMenu, ControlsMenu, ExitDialogue, Record, PointsTag;
+    //public GameObject FirstOptionsButton, CloseOptionsButton, FirstCloseButton, CloseCloseButton, AudioSlider, CloseAudioButton, VideoResolution, CloseVideoButton, FirstControlsButton, CloseControlsButton;
+    public GameObject CurrentStateMenu;
+    public List<GameObject> CurrentStateMenuButtons;
+    public List<GameObject> SelectedImages;
+    public bool ExistingList = false;
+    public int Index;
+    enum MenuStates { MainMenu,
+        HowToPlay,
+        Credits,
+        ExitGame,
+        Options,
+        Controls,
+        Audio,
+        Video
+    }
+    MenuStates CurrentState;
     // Start is called before the first frame update
     void Start()
     {
-        
+        CurrentState = MenuStates.MainMenu;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MenuStateMachine();
     }
-    #region Options
+    /*#region Options
     public void OpenOptions()
     {
         OptionsMenu.SetActive(true);
@@ -34,7 +50,7 @@ public class MenuWithKeyboard : MonoBehaviour
         EventSystem.current.SetSelectedGameObject(CloseOptionsButton);
     }
     #endregion
-    #region Audio
+    //#region Audio
     public void OpenAudio()
     {
         AudioMenu.SetActive(true);
@@ -48,7 +64,7 @@ public class MenuWithKeyboard : MonoBehaviour
         EventSystem.current.SetSelectedGameObject(CloseAudioButton);
     }
     #endregion
-    #region Video
+    //#region Video
     public void OpenVideo()
     {
         VideoMenu.SetActive(true);
@@ -62,7 +78,7 @@ public class MenuWithKeyboard : MonoBehaviour
         EventSystem.current.SetSelectedGameObject(CloseVideoButton);
     }
     #endregion
-    #region Controls
+    //#region Controls
     public void OpenControls()
     {
         ControlsMenu.SetActive(true);
@@ -82,7 +98,7 @@ public class MenuWithKeyboard : MonoBehaviour
         EventSystem.current.SetSelectedGameObject(FirstControlsButton);
     }
     #endregion
-    #region Exit
+    //#region Exit
     public void OpenExit()
     {
         ExitDialogue.SetActive(true);
@@ -95,5 +111,75 @@ public class MenuWithKeyboard : MonoBehaviour
         EventSystem.current.SetSelectedGameObject(null);
         EventSystem.current.SetSelectedGameObject(CloseCloseButton);
     }
-    #endregion
+    #endregion*/
+
+    public void MenuStateMachine()
+    {
+       switch (CurrentState)
+        {
+            case MenuStates.MainMenu:
+                MainMenu();
+                break;
+            case MenuStates.HowToPlay:
+
+                break;
+            case MenuStates.Credits:
+
+                break;
+            case MenuStates.ExitGame:
+
+                break;
+            case MenuStates.Options:
+
+                break;
+            case MenuStates.Controls:
+
+                break;
+            case MenuStates.Audio:
+
+                break;
+            case MenuStates.Video:
+
+                break;
+        }
+    }
+    
+    public void GetGameObject()
+    {
+        if(ExistingList==false)
+        {
+            Button[] ButtonArray;
+            CurrentStateMenu = gameObject.transform.Find(CurrentState.ToString()).gameObject;
+            ButtonArray = CurrentStateMenu.GetComponentsInChildren<Button>();
+
+            foreach (Button button in ButtonArray)
+            {
+                CurrentStateMenuButtons.Add(button.gameObject);
+                SelectedImages.Add(button.transform.Find("Selected").gameObject);
+            }
+            Index = 0;
+            ExistingList = true;
+        }
+        
+    }
+
+    public void CycleButtons()
+    {
+        if(ExistingList==true)
+        {
+            if(Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Index--;
+                if(Index<0)
+                {
+                    Index = CurrentStateMenuButtons.Count;
+                }
+            }
+        }
+    }
+
+    public void MainMenu()
+    {
+        GetGameObject();
+    }
 }
