735e27c602e71375f246165c90d1f38fd3b5bfaf
Daniele Comotti <comotti.daniele@gmail.com>
2020-06-13 11:19:19 +0200

    Modifiche alla UI del menù principale

---
+++ progetto comune prototipo/Assets/01-Scripts/CameraScreenResolution.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScreenResolution : MonoBehaviour
+{
+    public bool MaintainWidth = true;
+    float DefaultWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DefaultWidth = Camera.main.orthographicSize * Camera.main.aspect;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(MaintainWidth)
+        {
+            Camera.main.orthographicSize = DefaultWidth / Camera.main.aspect;
+        }
+        else
+        {
+
+        }
+    }
+}
