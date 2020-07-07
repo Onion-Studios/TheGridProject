5b8c340e7eed4ff3200456d143872dec4fbb3c47
Alex Silvestri <alexsilvestri.16@gmail.com>
2020-07-07 14:51:24 +0200

    Movimento tolto durante Intensity Reset

--- progetto comune prototipo/Assets/01-Scripts/Playerbehaviour.cs
+++ progetto comune prototipo/Assets/01-Scripts/Playerbehaviour.cs
@@ -93,7 +93,7 @@ public class Playerbehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (startEndSequence.starting == false && startEndSequence.ending == false && YS.active == false && SecretT.timeStopped == false)
+        if (startEndSequence.starting == false && startEndSequence.ending == false && YS.active == false && SecretT.timeStopped == false && intensityreset.intensityReset == false)
         {
             MovementHandler();
             if (movementState == "movingforward" || movementState == "movingback" || movementState == "movingleft" || movementState == "movingright")
