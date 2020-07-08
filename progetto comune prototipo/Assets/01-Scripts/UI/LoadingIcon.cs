253ca672a427d5c7e566fa9183f9911a9737f18c
Andrea Facchinetti <facchiandrea1510@gmail.com>
2020-07-08 18:08:24 +0200

    Implementata icona loading

---
+++ progetto comune prototipo/Assets/01-Scripts/UI/LoadingIcon.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingIcon : MonoBehaviour
+{
+    public Slider bar;
+    public float maxTime;
+    public float currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTime >= 0 && currentTime < 5)
+        {
+            currentTime += 1 * Time.deltaTime;
+            bar.value = currentTime / 5;
+        }
+        else
+        {
+            currentTime = 5;
+            bar.enabled = false;
+        }
+    }
+}
